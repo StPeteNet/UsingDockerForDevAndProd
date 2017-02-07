4ac43bf8a6192c723097db8a8fa31f5b04cc1712
Clayton Hunt <claytonhunt@outlook.com>
2017-02-07 04:42:32 -0500

    changed where sha is being used

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
-docker pull microsoft/aspnetcore-build@sha256:b60565f83a39984f634d61b6a0e21faf5650b19fb1ffc280fc6e47fdeb0290d9
-
 docker build -f ./src/Dockerfile --tag interactive-docker .
 
 docker run --rm --name interactive-docker -it -v %CD%\src:/app -t interactive-docker
