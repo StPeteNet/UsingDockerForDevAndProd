3d700f36af13ea8a2e49809e12935f8cc324c920
Clayton Hunt <claytonhunt@outlook.com>
2017-04-08 13:34:14 -0400

    fixed issue with container build

--- dev.bat
+++ dev.bat
@@ -1,10 +1,10 @@
-cd ./src
+cd ./src/using-docker.web
 
 docker pull microsoft/aspnetcore-build@sha256:80fe1d5a66ae4cf22d71b9e1003db16fde3ba5f9665589ff19b589078d76fc66
 
 docker build --rm --tag dotnet-development .
 
-cd ..
+cd ../..
 
 start http://localhost:3000/
 
